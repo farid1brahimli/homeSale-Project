92ccb5ae2d4251a5a4dca7b8b1df1a4e4701b3fc
farid1brahimli <farid1brahimli.bs@gmail.com>
2025-10-10 00:44:50 +0400

    aboutUs page the end

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -24,35 +24,53 @@ const About = () => {
       </section>
       <section className='about-third-sec'>
         <div className="third-container">
-          <h1></h1>
-          <h1></h1>
-          <div>
+          <h1 className='meet'>Meet out team</h1>
+          <h1 className='branch'>They are the best in the branch</h1>
+          <div className='images'>
             <div>
-              <img src="" alt="" />
-              <h1></h1>
-              <p></p>
+              <img src="./aboutMaria3.png" alt="" />
+              <h1>Will Smith</h1>
+              <p>Realtor</p>
             </div>
             <div>
-              <img src="" alt="" />
-              <h1></h1>
-              <p></p>
+              <img src="./aboutMaria1.png" alt="" />
+              <h1>Maria Smith</h1>
+              <p>Director</p>
             </div>
             <div>
-              <img src="" alt="" />
-              <h1></h1>
-              <p></p>
+              <img src="./aboutMaria2.png" alt="" />
+              <h1>Will Smith</h1>
+              <p>Realtor</p>
             </div>
           </div>
-          <div>
-            <div>
-              <p></p>
+          <div className='skills'>
+            <div className='skill'>
+              <img src="./aboutIcon1.png" alt="" />
+              <div>
+                <p>350</p>
+                <p>Apartments Sold</p>
+              </div>
             </div>
-             <div>
-              <p></p>
-            </div> <div>
-              <p></p>
-            </div> <div>
-              <p></p>
+            <div className='skill'>
+              <img src="./aboutIcon2.png" alt="" />
+              <div>
+                <p>1278</p>
+                <p>Happy Clients</p>
+              </div>
+            </div>
+            <div className='skill'>
+              <img src="./aboutIcon3.png" alt="" />
+              <div>
+                <p>25</p>
+                <p>Years of experience</p>
+              </div>
+            </div>
+            <div className='skill'>
+              <img src="./aboutIcon4.png" alt="" />
+              <div>
+                <p>17</p>
+                <p>Awards Wone</p>
+              </div>
             </div>
           </div>
         </div>
